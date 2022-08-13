99d881d1f5d6b93d8d45ee1fa7e3fd511f524cc2
Shawn Dempsey <shawn@shawndempsey.ca>
2022-08-13 10:01:00 -0600

    adding lettuce to salad

--- src/Salad.php
+++ src/Salad.php
@@ -2,10 +2,15 @@
 namespace Salad;
 
 
+use Lettuce\Lettuce;
+
 class Salad
 {
+    private $lettuce;
+
     public function __construct()
     {
         echo "Salad v1.1.0\n";
+        $this->lettuce = new Lettuce();
     }
 }
